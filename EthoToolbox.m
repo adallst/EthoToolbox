7cd80b6af51b54968f98a243593c33d3e5611b75
Geoffrey K. Adams <geoff.adams@gmail.com>
2017-03-09 19:43:28 -0500

    Setup code

--- EthoToolbox.m
+++ EthoToolbox.m
@@ -1,6 +1,23 @@
 function EthoToolbox(varargin)
 
 if ismember('setup', varargin)
+    % Add EthoToolbox to the path
+    this_dir = fileparts(mfilename('fullpath'));
+    util_dir = fullfile(this_dir, 'util');
+    cur_path = addpath(util_dir);
+    
+    message = EthoMakeMessenger;
+    message('Adding EthoToolbox to the path and setting up local info.');
+    
+    etho_path = etho_genpath(this_dir);
+    path(etho_path, cur_path);
+    savepath;
+    
     p = EthoPaths;
+    mkdir(p.settings);
+    mkdir(p.profiles);
+    mkdir(p.profiles, 'default');
+    
+    message('Set up default screen info now?');
     
 end
--- util/etho_genpath.m
+++ util/etho_genpath.m
@@ -2,16 +2,12 @@ function p = etho_genpath(d)
 % A slighly customized version of genpath which excludes all directories
 % beginning with '.'.
 
-error('Not working right, stop now and fix.');
+%error('Not working right, stop now and fix.');
 % Need to use builtin genpath rather than recursive dir, because recursive dir
 % seems to follow symlinks into infinite loops
 
-listing = dir(d);
-listing_entries = {listing.name};
-subdirs = listing_entries([listing.isdir] & ~strncmp(listing_entries, '.', 1));
-subdirs = fullfile(d, subdirs);
-subpaths = cell(size(subdirs));
-for i = 1:numel(subdirs)
-    subpaths{i} = etho_genpath(subdirs{i});
-end
-p = strjoin([{d}, subpaths],pathsep);
+fullset = genpath(d);
+subdirs = strsplit(fullset, pathsep);
+isNotHidden = cellfun(@isempty, strfind(subdirs, [filesep '.']));
+subdirs = subdirs(isNotHidden);
+p = strjoin(subdirs, pathsep);
